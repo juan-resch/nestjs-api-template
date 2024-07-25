33d0e9de479f4b2bc4c4c2d8ae984d71a4b43dff
Juan Resch <55303897+juan-resch@users.noreply.github.com>
2024-07-24 22:09:51 -0300

    remove unused EnvModule from user.module

--- src/modules/user/infra/modules/user.module.ts
+++ src/modules/user/infra/modules/user.module.ts
@@ -4,10 +4,9 @@ import CreateUserUseCase from '../../application/use-cases/create/create-user.us
 import CreateUserController from '../controllers/create/create-user.controller'
 
 import { DatabaseModule } from '@/shared/db/database.module'
-import { EnvModule } from '@/shared/env/env.module'
 
 @Module({
-  imports: [DatabaseModule, EnvModule],
+  imports: [DatabaseModule],
   providers: [CreateUserUseCase],
   controllers: [CreateUserController],
 })
